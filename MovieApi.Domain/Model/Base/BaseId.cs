7a4c3745d5bdbcdf2018b5cf9af73fd9e744f7a9
Edgar Jimenez <ejimenez@mardom.com>
2022-11-25 01:00:05 -0400

    feat: models created

--- MovieApi.Domain/Model/Base/BaseId.cs
+++ MovieApi.Domain/Model/Base/BaseId.cs
@@ -1,7 +1,9 @@
-﻿namespace MovieApi.Domain.Model.Base
-{
-    public class BaseId
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Domain.Model.Base;
+public class BaseId
     {
+        [Key]
         public int Id { get; set; }
     }
-}
+
