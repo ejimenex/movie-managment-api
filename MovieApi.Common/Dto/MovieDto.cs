4108e78a70488e68bef8d7b36010bfa67bfc78e8
Edgar Jimenez <ejimenez@mardom.com>
2022-11-25 02:08:20 -0400

    feat: configuring Dto part 1

---
+++ MovieApi.Common/Dto/MovieDto.cs
@@ -0,0 +1,17 @@
+﻿namespace MovieApi.Common.Dto
+{
+    public class MovieDto: BaseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string UrlMovie { get; set; }
+        public int YearOfMovie { get; set; }
+        public int MinimalAge { get; set; }
+        public string Quality { get; set; }
+        public int TotalHourDuration { get; set; }
+        public string ImageUrl { get; set; }
+        public int TotalMinuteDuration { get; set; }
+        public decimal RankingAvg { get; set; }
+        public IEnumerable<MovieReviewDto> Reviews { get; set; }
+    }
+}
---
+++ MovieApi.Common/Dto/MovieReviewDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Common.Dto
+{
+    public class MovieReviewDto: BaseDto
+    {
+        public int MovieId { get; set; }
+        public int Ranking { get; set; }
+        public string Commentary { get; set; }
+        public virtual MovieDto Movie { get; set; }
+    }
+}
